83eba9cb263a9f3476338b207189da2f3fe0e2b4
Christiaan Landman <chriz.ek@gmail.com>
2022-09-19 20:24:35 +0200

    .

--- store/setup/supabase.ts
+++ store/setup/supabase.ts
@@ -7,7 +7,3 @@ const supabaseAnonKey =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqY3F1ZnBueWpidXdqZ2xwdXZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjExOTg0NTIsImV4cCI6MTk3Njc3NDQ1Mn0.65tZctEviMKFj2EkynvnWnpJ3F0x_Z8n4OB2fdyOmwU';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
-
-console.log(supabaseUrl);
-console.log(supabaseAnonKey);
-console.log('THis happenede/');
